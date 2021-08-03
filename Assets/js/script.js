cac2da6a57d204304102b889d5e0c2a6043f50d5
Jacob Adamson <jakeladamson@gmail.com>
2021-08-03 15:29:05 -0600

    setup structure for questions

---
+++ Assets/js/script.js
@@ -0,0 +1,36 @@
+// make a btn and and event listener
+
+//research making a timer
+
+//declare variables
+
+//make an array of objects
+//(question: question, question: choices, key:value pair )
+var questions = [
+  {
+    question: "Commonly used data types DO NOT include:",
+    choices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+    answer: "3. alerts",
+  },
+  {
+    question: "Commonly used data types DO NOT include:",
+    choices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+    answer: "3. alerts",
+  },
+  {
+    question: "Commonly used data types DO NOT include:",
+    choices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+    answer: "3. alerts",
+  },
+  {
+    question: "Commonly used data types DO NOT include:",
+    choices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+    answer: "3. alerts",
+  },
+  {
+    question: "Commonly used data types DO NOT include:",
+    choices: ["1. strings", "2. booleans", "3. alerts", "4. numbers"],
+    answer: "3. alerts",
+  },
+];
+//Question index 0 ++, then next question
