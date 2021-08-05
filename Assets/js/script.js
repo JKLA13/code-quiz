bd3d978705fa9c14120727b3c7a5cc583d50570b
Jacob Adamson <jakeladamson@gmail.com>
2021-08-04 22:53:51 -0600

    worked css and js to stop listener for click event - had weird glitch

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,9 +1,11 @@
 //declare variables
 
-var startTimer = document.getElementById("timerStart")
+var startTimer = document.getElementById("timerStart");
 var timerEl = document.getElementById("countdown");
 var questionSection = document.getElementById("questionSection");
-var ulCreate = document.createElement("ul");
+var ulCreate = document.createElement("div");
+var messages = document.getElementById("messages");
+var clickStop = document.getElementById("clickStop");
 
 // declare timer variables
 var timeLeft = 75;
@@ -31,14 +33,14 @@ var questions = [
       "3. parentheses",
       "4. square brackets",
     ],
-    answer: "3. parentheses",
+    answer: "2. curly brackets",
   },
   {
     question: "Arrays in JavaScript can be used to store______.",
     choices: [
       "1. numbers and strings",
       "2. other arrays",
-      "3. cooleans",
+      "3. booleans",
       "4. all of the above",
     ],
     answer: "4. all of the above",
@@ -58,136 +60,106 @@ var questions = [
       "3. for loops",
       "4. console log",
     ],
-    answer: "3. console log",
+    answer: "4. console log",
   },
 ];
 
 // need to display question upon start quiz button click, and switch question upon answers submitted
 
-
 ///3 different screens/setups
 
-function displayQuestion(questionIndex) {
-    questionSection.innerHTML = "";
-    ulCreate.innerHTML = "";
+function displayQuestion() {
+  clickStop.classList.remove("show");
+  clickStop.classList.add("hide");
 
-    // Question index 0 ++, then next question
-    // need for loops
-    for  (var i = 0; i < questions.length; i++){
-      var playerQuestions = questions[questionIndex].question;
-      var playerAnswers = questions[questionIndex].choices;
-      questionSection.textContent = playerQuestions;
+  questionSection.innerHTML = "";
+  ulCreate.innerHTML = "";
 
-    }
+  var playerQuestions = questions[questionIndex].question;
+  questionSection.textContent = playerQuestions;
+
+  questions[questionIndex].choices.forEach(function (newChoice) {
+    var newItem = document.createElement("p");
+    newItem.textContent = newChoice;
+    questionSection.appendChild(ulCreate);
+    newItem.addEventListener("click", answerCheck);
+    ulCreate.appendChild(newItem);
+    questionSection.appendChild(ulCreate);
+  });
 }
-
-playerAnswers.forEach(function (newChoice) {
-  var newItem = document.createElement("li");
-  newItem.textContent = newChoice;
-  questionSection.appendChild(ulCreate);
-  ulCreate.appendChild(newItem);
-  newItem.addEventListener("lick", (answerCheck));
-
-});
 
 // setup if -  conditional statements to check which user answer picked
 function answerCheck(event) {
+  clickStop.classList.remove("hide");
+  clickStop.classList.add("show");
+
   var elCheck = event.target;
 
-  if (elCheck.matches("li")) {
-    var newSection = document.createElement("div");
-    newSection.setAttribute("id", "newSection");
-  }
-  
   if (elCheck.textContent == questions[questionIndex].answer) {
     wins++;
-    userMessage.textContent = "You got the right answer: " + questions[questionIndex].answer) {
-
-    } else {
-      timeLeft = timeLeft - wrongPenalty;
-      newSection.textContent = "You guessed wrong!  Here's the correct answer: " + questions[questionIndex].answer;
-    }
-  }
-
-  questionIndex ++;
-
-  if (questionIndex >= questions.length) {
-    stopQuiz();
-    newSection.textContent = "Quiz Over!" + " " + "Your wins: " + wins + " out of" + questions.length + "!";
-
+    messages.innerHTML = "You got the right answer! ";
+    //when answers are wrong need to subtract time from clock
   } else {
-    displayQuestion(questionIndex);
+    timeLeft = timeLeft - wrongPenalty;
+    messages.innerHTML = "You guessed wrong!";
   }
-  questionSection.appendChild(newSection);
+
+  questionIndex++;
+
+  if (questionIndex > 4) {
+    messages.innerHTML = "";
+    stopQuiz();
+  } else {
+    setTimeout(function () {
+      messages.innerHTML = "";
+      displayQuestion();
+    }, 2000);
+  }
+}
 
 // Need End section
 function stopQuiz() {
   questionSection.innerHTML = "";
   timerEl.innerHTML = "";
 
-    var newH1 = document.createElement("h1");
-    newH1.setAttribute("id", "newH1");
-    newH1.textContent = "Quiz over!";
+  var newH1 = document.createElement("h1");
+  newH1.setAttribute("id", "newH1");
+  newH1.textContent = "Quiz over!";
 
-    questionSection.appendChild(newH1);
+  questionSection.appendChild(newH1);
 
-    var newP = document.createElement("p");
-    newP.setAttribute("id", "newP");
-    questionSection.appendChild(newP);
+  var newP = document.createElement("div");
+  newP.setAttribute("id", "newP");
+  questionSection.appendChild(newP);
 
   if (timeLeft >= 0) {
     var secondsLeft = timeLeft;
-    var newP2 = document.createElement("p");
+    var newP2 = document.createElement("div");
     clearInterval(timerHold);
-    newP.textContent = "Your final score: " + secondsLeft;
+    newP.textContent = "Your final score: " + wins;
     newSection.appendChild(newP2);
   }
-
 }
-
 
 // make a btn and and event listener
 //need to display Coding Quiz title and Start Quiz btn
 
-timerEl.addEventListener("click", function () {
-    if (timerHold === 0) {
-        timerHold = setInterval(function () {
-            timeLeft --;
-            timerEl.textContent = timeLeft;
+startTimer.addEventListener("click", function () {
+  displayQuestion(questionIndex);
 
-            if (timeLeft <= 0) {
-                clearInterval(timerHold);
-                stopQuiz();
-                timerEl.textContent = "All out of time :-("
-            }
-        }, 1000);
-    } 
-}
+  if (timerHold === 0) {
+    timerHold = setInterval(function () {
+      timeLeft--;
+      timerEl.textContent = timeLeft;
 
+      if (timeLeft <= 0) {
+        clearInterval(timerHold);
+        stopQuiz();
+        timerEl.textContent = "All out of time :-(";
+      }
+    }, 1000);
+  }
+});
 
-
+TODO:
 //When questions or game is over, we need a text input for initials that pulls high scores from local storage
-
-
-
-
-// timer functions
-// set interval
-//when answers are wrong need to subtract time from clock
-
-// function ticktock() {
-//   time--;
-//   timerEl.textContent = time;
-
-//   if (time <= 0) {
-//     stopQuiz(); //TODO: build stop quiz function
-//   }
-// }
-
-// setInterval(ticktock, 1000);
-
-
-
-
-
-
